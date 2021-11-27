b27e05f82760ed64f47efe0f3c1272d8594ea548
Sidharth Pandey <sidp919@gmail.com>
2021-11-27 19:03:56 +0530

    Print all Prime numbers less than or equal to n

--- Basic problems/Part_B/d_sieve_of_eratosthenes.py
+++ Basic problems/Part_B/d_sieve_of_eratosthenes.py
@@ -15,10 +15,16 @@ def printPrimeNumbers(n):
     primeNumList[0] = False # 0 is not a Prime number
     primeNumList[1] = False # 1 is not a Prime number
     length = int(n/2)
-    for i in range(2,length):  # len=n/2 bcoz j=2*i so, we only traverse till n/2 becoz at n/2, j = n
+    for i in range(2,length):  # length=n/2 bcoz j=2*i so, we only traverse till n/2 becoz at n/2, j = n
         if primeNumList[i]==True:
             for j in range(2*i,n+1,i):  # Striding: loop from j=2*i to j=n while skipping i elements in between.
                 primeNumList[j]=False   # multiple of a primeNumber is not a primeNumber(ex. 4, 6, 8... are multiples of 2)
-    print([ind for ind in range(len(primeNumList)) if primeNumList[ind]==True]) # print primeNumbers with list comprehension
+    print(f"All Prime numbers less than or equal to {n}: {[ind for ind in range(len(primeNumList)) if primeNumList[ind]==True]}") # print primeNumbers with list comprehension
 
 printPrimeNumbers(n)
+
+# OUTPUT:
+# =======
+# Enter a number: 50
+#
+# All Prime numbers less than or equal to 50: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
