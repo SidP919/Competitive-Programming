b961f99ae8cb8d88fce44752d7172e9411ec5e15
Sidharth Pandey <sidp919@gmail.com>
2021-11-27 18:43:10 +0530

    Sum of first n natural numbers

--- Basic problems/Part_B/a_sum_of_first_n_numbers.py
+++ Basic problems/Part_B/a_sum_of_first_n_numbers.py
@@ -10,10 +10,18 @@ print()
 sum = 0
 for i in range(n+1):
     sum = sum + i
-print(f"with BF: sum = {sum}")
+print(f"BF: with for loop, result = {sum}")
 print()
 
-# Approach #2: Gauss's sum:
+# Approach #2: BA(Better Approach): Gauss's sum:
 sum2 = (n*(n+1))/2
-print(f"with Gauss's sum: {sum2}")
+print(f"BA: with Gauss's sum, result = {sum2}")
 print()
+
+# OUTPUT:
+# =======
+# Enter a number: 50
+
+# BF: result = 1275
+
+# BA: with Gauss's sum, result = 1275.0
